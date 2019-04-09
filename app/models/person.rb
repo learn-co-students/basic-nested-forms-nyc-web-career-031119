81fb5f918c33a41ac63b752aefdba478a66b2415
J-Agens <johntagens@gmail.com>
2019-04-09 10:40:03 -0400

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,7 @@
 class Person < ActiveRecord::Base
   has_many :addresses
 
+  # If you want to avoid duplicates, you have to write out the
+  # x_attributes= method as demonstrated at the end of the readme.
+  accepts_nested_attributes_for :addresses
 end
